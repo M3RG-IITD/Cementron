a31ae35d3edee9bd7821e89ac61855d6905925a6
zakidotai <mohdzaki1995@gmail.com>
2022-11-06 21:39:32 +0530

    updated codes for training models

--- scripts/train_folds.py
+++ scripts/train_folds.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import sys
 import numpy as np
 import os, json, cv2, random
@@ -17,15 +18,26 @@ from detectron2.projects import point_rend
 from detectron2.data.datasets import register_coco_instances
 
 
-fold = sys.argv[1]
-start = int(sys.argv[2])
-finish = int(sys.argv[3])
+# fold = sys.argv[1]
+# start = int(sys.argv[2])
+# finish = int(sys.argv[3])
+
+parser = ArgumentParser()
+parser.add_argument('--fold', required=True, type=int)
+parser.add_argument('--start', required=True, type=int)
+parser.add_argument('--finish', required=True, type=int)
+
+args = parser.parse_args()
+
+fold = args.fold
+start = args.start
+finish = args.finish
 
 print(f'Training fold: {fold} of 4')
 
 register_coco_instances(name = f"datatrain_{fold}", metadata = {}, 
-                        json_file = f"/home/civil/phd/cez198233/cement/Part_3/master_folder/fourier_o/cement_coco_train_fold_{fold}.json",
-                        image_root = "/home/civil/phd/cez198233/cement/Part_3/master_folder/fourier_o")
+                        json_file = f"/data/literature_images/cement_coco_train_fold_{fold}.json",
+                        image_root = "/data/literature_images")
 
 
 train_meta = DatasetCatalog.get(f'datatrain_{fold}')
@@ -34,7 +46,7 @@ coco_metadata = MetadataCatalog.get("coco_2017_val")
 
 cfg = get_cfg()
 
-cfg.OUTPUT_DIR = f'/home/civil/phd/cez198233/scratch/cement/detectron_out/fold_{fold}'
+cfg.OUTPUT_DIR = f'./detectron_out/fold_{fold}'
 
 point_rend.add_pointrend_config(cfg)
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
@@ -42,24 +54,28 @@ cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rc
 cfg.DATASETS.TRAIN = (f"datatrain_{fold}",)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 1
-# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training 
 
-cfg.MODEL.WEIGHTS = f'/scratch/civil/phd/cez198233/cement/detectron_out/fold_{fold}/model_final.pth'
+# if training from starting, uncomment below line
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training 
 
+# if training from checkpoint, uncomment below line
+# cfg.MODEL.WEIGHTS = f'./detectron_out/fold_{fold}/model_final.pth'
+
+# The for loop logs the model result every 10 epochs
 epochs = list(np.arange(start,finish,10)) #1010
 print('Logging at intervals:',epochs)
 for epoch in epochs:
     cfg.SOLVER.IMS_PER_BATCH = 1
-    cfg.SOLVER.BASE_LR = 2e-5  # pick a good LR
+    cfg.SOLVER.BASE_LR = 2e-5
     cfg.SOLVER.MAX_ITER = int(epoch)    
-    cfg.SOLVER.STEPS = []        # do not decay learning rate
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8   # faster, and good enough for this toy dataset (default: 512)
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-    # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+    cfg.SOLVER.STEPS = []        
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8   
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  
     cfg.MODEL.DEVICE='cpu'
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
 
     trainer = DefaultTrainer(cfg) 
-    trainer.resume_or_load(resume=True)
+    # trainer.resume_or_load(resume=True) # if training from checkpoint
+    trainer.resume_or_load(resume=False) # if training from starting
     trainer.train()
--- scripts/train_single.py
+++ scripts/train_single.py
@@ -27,7 +27,6 @@ parser.add_argument('--label', required=True, type=str)
 parser.add_argument('--fold', required=True, type=int)
 parser.add_argument('--start', required=True, type=int)
 parser.add_argument('--finish', required=True, type=int)
-parser.add_argument('--radius', required=True, type=int)
 
 args = parser.parse_args()
 
@@ -35,47 +34,47 @@ dset = args.label
 fold = args.fold
 start = args.start
 finish = args.finish
-radius = args.radius
 
 print(f'Training fold: {fold} of 4')
 
-register_coco_instances(name = f"datatrain_{dset}_{fold}_{radius}", metadata = {}, 
-                        json_file = f"/home/civil/phd/cez198233/cement/Part_3/master_folder/fourier_o/cement_coco_train_{dset}_fold_{fold}.json",
-                        image_root = f"/home/civil/phd/cez198233/cement/Part_3/master_folder/fourier_{radius}")
+register_coco_instances(name = f"datatrain_{dset}_{fold}", metadata = {}, 
+                        json_file = f"/data/literature_images/cement_coco_train_{dset}_fold_{fold}.json",
+                        image_root = f"/data/literature_images")
 
-train_meta = DatasetCatalog.get(f'datatrain_{dset}_{fold}_{radius}')
+train_meta = DatasetCatalog.get(f'datatrain_{dset}_{fold}')
 
 coco_metadata = MetadataCatalog.get("coco_2017_val")
 
 cfg = get_cfg()
 
-cfg.OUTPUT_DIR = f'/home/civil/phd/cez198233/scratch/cement/detectron_out/fold_{dset}_{fold}_{radius}'
+cfg.OUTPUT_DIR = f'./detectron_out/fold_{dset}_{fold}'
 
 point_rend.add_pointrend_config(cfg)
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 
-cfg.DATASETS.TRAIN = (f"datatrain_{dset}_{fold}_{radius}",)
+cfg.DATASETS.TRAIN = (f"datatrain_{dset}_{fold}",)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 1
-# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training 
+# if training from starting, uncomment below line
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training 
 
-cfg.MODEL.WEIGHTS = f'/scratch/civil/phd/cez198233/cement/detectron_out/fold_{dset}_{fold}_{radius}/model_final.pth'
+# if training from checkpoint, uncomment below line
+cfg.MODEL.WEIGHTS = f'./detectron_out/fold_{dset}_{fold}/model_final.pth'
 
-
+# The for loop logs the model result every 10 epochs
 epochs = list(np.arange(start,finish,10)) #1010
 print('Logging at intervals:',epochs)
 for epoch in epochs:
     cfg.SOLVER.IMS_PER_BATCH = 1
-    cfg.SOLVER.BASE_LR = 2e-5  # pick a good LR
+    cfg.SOLVER.BASE_LR = 2e-5  
     cfg.SOLVER.MAX_ITER = int(epoch)    
-    cfg.SOLVER.STEPS = []        # do not decay learning rate
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8   # faster, and good enough for this toy dataset (default: 512)
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (allite). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-    # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+    cfg.SOLVER.STEPS = []     
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8   
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  
     cfg.MODEL.DEVICE='cpu'
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
-
     trainer = DefaultTrainer(cfg) 
-    trainer.resume_or_load(resume=True)
+    # trainer.resume_or_load(resume=True) # if training from checkpoint
+    trainer.resume_or_load(resume=False) # if training from starting
     trainer.train()
